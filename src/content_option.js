92f5ed1f391dff74d82d654584902e55b70e1191
Aniket Mishra <aniketdebu@hotmail.com>
2024-08-14 23:29:20 +0530

    portfolio commit

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "DEBU";
+const meta = {
+    title: "Aniket Mishra",
+    description: "I’m Aniket Mishra FrontEnd Developer",
+};
+
+const introdata = {
+    title: "I’m Aniket Mishra",
+    animated: {
+        first: "I like coding",
+        second: "I code to build websites",
+        third: "I develop Database & API based web applications",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1723293782071-061dd673d8e9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "'I'm a moderately skilled Web Developer with a wealth of diverse life and technical experience that I bring to every project.I follow form as well as function.I know that the aesthetics of effective design combined with application speed, security and reliability combine to create a great user experience.I am skilled in Server and Query Management.I get indulged in PVP online games Counter Strike & Valorant. ",
+};
+const worktimeline = [{
+        jobtitle: "Scrims Manager",
+        where: "Echoverse Esports (INDIA)",
+        date: "2024 (Present)",
+    },
+    {
+        jobtitle: "Full Stack Developer intern",
+        where: "Unified Mentor",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 80,
+    },
+    {
+        name: "MongoDB",
+        value: 75,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "ReactJS",
+        value: 60,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+    {
+        name: "SpringBoot",
+        value: 50,
+    }
+];
+
+const services = [{
+        title: "FrontEnd Design",
+        description: "",
+    },
+    {
+        title: "Web Apps",
+        description: "",
+    },
+    {
+        title: "Web Design",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aniketdebu@hotmail.com",
+    YOUR_FONE: "7682966499",
+    description: "For any project ideas feel free to contact I am open to freelancing as well.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_68x0wfk",
+    YOUR_TEMPLATE_ID: "template_ajtssqq",
+    YOUR_USER_ID: "",
+};
+
+const socialprofils = {
+    github: "https://github.com/DienDead?tab=overview&from=2021-12-01&to=2021-12-31",
+    facebook: "https://www.facebook.com/share/JG2bMQEJ3AY1gSyT/?mibextid=qi2Omg",
+    linkedin: "https://www.linkedin.com/in/aniket-mishra-005150279/",
+    twitter: "https://x.com/diendead?t=G4kqnsyDzD4dcMenCDd4gA&s=08",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
